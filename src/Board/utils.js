f93facd72a711a823fee150e253431b18e05b999
karelnguyen <nguyenkar@seznam.cz>
2022-04-03 14:51:24 +0200

    Clear package

--- src/Board/utils.js
+++
@@ -1,5 +0,0 @@
-export var randomStartingNumber = function (startingNumbers) {
-    if (startingNumbers === void 0) { startingNumbers = [2, 4]; }
-    var randomNumber = startingNumbers[Math.floor(Math.random() * startingNumbers.length)];
-    return randomNumber;
-};
